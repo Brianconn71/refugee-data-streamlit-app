ea2e4f792c89d8553bdae63f81ec5f69e6c45c97
Brian Connolly <brian.connolly71@gmail.com>
2024-07-21 19:50:05 +0100

    finished coding and checking first page and function

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import streamlit as st
-import pandas as import pd
+import pandas as  pd
 import plotly.express as px
 
 
 # load the data
-data = pd.read_csv("data/asylum_decisions.csv")
+data = pd.read_csv("data/asylum-decisions.csv")
 
 def get_asylum_counts(df, group_by_column):
     return df.groupby([group_by_column])[['Recognized decisions', 'Rejected decisions', 'Total decisions']].sum().reset_index()
@@ -13,19 +13,53 @@ def get_asylum_counts(df, group_by_column):
 def page_overview():
     st.subheader("Global Asylum Decisions by Year Range (Choose Range)")
     # data viz code below
-    year_filter = st.slider("Year Range", int(data['Year'].min()), int(data['Year'].max()), (int(data['Year'].min()), int(data['Year'].max())))
+    year_min = int(data['Year'].min())
+    year_max = int(data['Year'].max())
+    year_filter = st.slider("Year Range", year_min, year_max, (year_min, year_max))
     
     filtered_data = data[(data['Year'] >= year_filter[0]) & (data['Year'] <= year_filter[1])]
     
-    asylum_counts = get_asylum_counts(filtered_data, 'Country of Asylum')
+    asylum_counts = get_asylum_counts(filtered_data, 'Country of asylum')
     
-    top_countries = asylum_counts.sort_values(by='Total Decisions', ascending=False).head(10)
+    top_countries = asylum_counts.sort_values(
+        by='Total decisions', ascending=False
+    ).head(10)
     
-    fig_bar = px.bar(top_countries, x="Total Decisions", y="Country of Asylum", orientation="h", title="Top 10 countries by Total Asylum Decisions", color="Total Decisions", color_continuous_scale=px.colors.sequential.Yl0rRd)
+    fig_bar = px.bar(
+        top_countries, 
+        x="Total decisions", 
+        y="Country of asylum", 
+        orientation="h", 
+        title="Top 10 countries by Total Asylum Decisions", 
+        color="Total decisions", 
+        color_continuous_scale=px.colors.sequential.YlOrRd
+    )
     
-    fig_bar.update_layout(showlegend=False, height=400, yaxis={'categoryorder':'total ascending'})
+    fig_bar.update_layout(
+        showlegend=False, 
+        height=400, 
+        yaxis={'categoryorder':'total ascending'}
+    )
     fig_bar.update_coloraxes(showscale=False)
     st.plotly_chart(fig_bar)
+    
+    top_countries_origins = filtered_data[
+        filtered_data["Country of asylum"].isin(top_countries['Country of asylum'])
+    ]
+    
+    fig_sunburst = px.sunburst(
+        top_countries_origins, 
+        path=["Country of asylum", "Country of origin"], 
+        values="Total decisions", 
+        title="Top 10 countries by Origin Breakdown", 
+        color='Total decisions', 
+        color_continuous_scale=px.colors.qualitative.Bold
+    )
+    
+    fig_sunburst.update_layout(height=600, showlegend=False)
+    
+    fig_sunburst.update_coloraxes(showscale=False)
+    st.plotly_chart(fig_sunburst)
 
 
 # Second Page setup - country specific with barchart
